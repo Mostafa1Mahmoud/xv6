d744087dab7c835626b3e501a53f79fc1aee5577
Mohamed Ali <76550923+mohamed-ali-5050@users.noreply.github.com>
2021-01-27 01:51:20 +0200

    Update testinfo.c

--- testinfo.c
+++ testinfo.c
@@ -8,12 +8,11 @@ int main(int argc, char *argv[]){
   if (argc != 1){
   in = atoi(argv[1]);
   }
-  
   if (in < 0) {
       while(1){
     getpinfo(&ps);
     printf(1, "\nPID\t|\tUSED?\t|\tTickets\t|\tticks\n");
-    for (int i = 0; i < sizeof(ps.pid)/sizeof(ps.pid[0]); i++)
+    for (int i = 0; i < NPROC; i++)
     {
       if(ps.pid[i])
         printf(1, "%d\t|\t%d\t|\t%d\t|\t%d\n", ps.pid[i], ps.inuse[i], ps.tickets[i], ps.ticks[i]);
@@ -21,12 +20,11 @@ int main(int argc, char *argv[]){
     sleep(150);
   }
   }
-  
   else {    
           while(in--){
            getpinfo(&ps);
            printf(1, "\nPID\t|\tUSED?\t|\tTickets\t|\tticks\n");
-          for (int i = 0; i < sizeof(ps.pid)/sizeof(ps.pid[0]); i++)
+          for (int i = 0; i < NPROC ; i++)
              {
            if(ps.pid[i])
         printf(1, "%d\t|\t%d\t|\t%d\t|\t%d\n", ps.pid[i], ps.inuse[i], ps.tickets[i], ps.ticks[i]);
@@ -34,7 +32,6 @@ int main(int argc, char *argv[]){
     sleep(150);
   }
   }
-  
   exit();
   return 0;
 }
