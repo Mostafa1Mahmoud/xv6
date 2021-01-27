caf79366a3f9e2bda98542adadf2c00947279254
Mohamed Ali <76550923+mohamed-ali-5050@users.noreply.github.com>
2021-01-27 15:55:16 +0200

    Update pstat.h

--- pstat.h
+++ pstat.h
@@ -8,6 +8,7 @@ struct pstat {
   int tickets[NPROC]; // the number of tickets this process has
   int pid[NPROC];     // the PID of each process
   int ticks[NPROC];   // the number of ticks each process has accumulated
+  char name[NPROC][16]; // name of each process;
 };
 
 #endif // _PSTAT_H_
