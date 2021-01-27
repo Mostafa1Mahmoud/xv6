7438e617c47a46e79a90391fbfbd77273eee4864
Mohamed Ali <76550923+mohamed-ali-5050@users.noreply.github.com>
2021-01-27 15:50:00 +0200

    Update testinfo.c

--- testinfo.c
+++ testinfo.c
@@ -11,27 +11,28 @@ int main(int argc, char *argv[]){
   if (in < 0) {
       while(1){
     getpinfo(&ps);
-    printf(1, "\nPID\t|\tUSED?\t|\tTickets\t|\tticks\n");
+    printf(1, "\nPID\t|\tUSED?\t|\tTickets\t|\tticks\t|\tName\n");
     for (int i = 0; i < NPROC; i++)
-    {
+    {   
       if (ps.inuse[i])
-        printf(1, "%d\t|\t%d\t|\t%d\t|\t%d\n", ps.pid[i], ps.inuse[i], ps.tickets[i], ps.ticks[i]);
+        printf(1, "%d\t|\t%d\t|\t%d\t|\t%d\t|\t%s\n", ps.pid[i], ps.inuse[i], ps.tickets[i], ps.ticks[i],ps.name[i]);
     }
-    sleep(150);
+    sleep(100);
   }
   }
   else {    
           while(in--){
            getpinfo(&ps);
-           printf(1, "\nPID\t|\tUSED?\t|\tTickets\t|\tticks\n");
+            printf(1, "\nPID\t|\tUSED?\t|\tTickets\t|\tticks\t|\tName\n");
           for (int i = 0; i < NPROC; i++)
              {
            if (ps.inuse[i])
-        printf(1, "%d\t|\t%d\t|\t%d\t|\t%d\n", ps.pid[i], ps.inuse[i], ps.tickets[i], ps.ticks[i]);
+        printf(1, "%d\t|\t%d\t|\t%d\t|\t%d\t|\t%s\n", ps.pid[i], ps.inuse[i], ps.tickets[i], ps.ticks[i],ps.name[i]);
     }
-    sleep(150);
+    sleep(100);
   }
   }
   exit();
   return 0;
 }
+  
