9c46e5a4e3a3da52aa9fa6b8cc8f154ec63ba425
Mohamed Ali <76550923+mohamed-ali-5050@users.noreply.github.com>
2021-01-27 15:50:52 +0200

    Update nullprog.c

--- nullprog.c
+++ nullprog.c
@@ -5,18 +5,8 @@
 int
 main(int argc, char *argv[])
 {
-   int ppid = getpid();
-   if (fork() == 0) {
-      uint * nullp = (uint*)0;
-      printf(1, "null dereference: ");
-      printf(1, "%x %x\n", nullp, *nullp);
-      // this process should be killed
-      printf(1, "TEST FAILED\n");
-      kill(ppid);
-      exit();
-   } else {
-      wait();
-   }
-   printf(1, "TEST PASSED\n");
+   int *pi = NULL ;
+   printf(1,"I will trap now: null pointer value: ");
+   printf(1, " %d\n" , *pi);
    exit();
 }
